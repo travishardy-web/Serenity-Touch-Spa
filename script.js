24974cd2340751e4ca45ccac1c3c5e8106f1019b
travishardy-web <travishardy055@gmail.com>
2026-08-17 15:59:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 
+  <!-- JAVASCRIPT -->
+<script>
+<script src="script.js"></script>
+
+    // Automatic copyright year
+    document.getElementById("year").textContent =
+      new Date().getFullYear();
+
+
+    // Smooth navigation
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+      link.addEventListener("click", function(e) {
+
+        const target =
+          document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+
+          e.preventDefault();
+
+          target.scrollIntoView({
+            behavior: "smooth"
+          });
+
+        }
+
+      });
+
+    });
+	<script>
+  document.getElementById("year").textContent = new Date().getFullYear();
+</script>
+
+  </script>
